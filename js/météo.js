b9d124a545f6e97f1826c5f3a7fa3f8702ffbef4
Andy Ramaroson <andyramaroson@gmail.com>
2024-04-03 16:48:54 +0200

    (feat) create request OpenweatherApi and get info to display

--- js/météo.js
+++ js/météo.js
@@ -1,1 +1,20 @@
-// Create file for data weather
+// Create api for data weather
+
+const apiKey = ""
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bordeaux";
+
+async function checkWeather() {
+  try {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    const data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name
+    document.querySelector(".country").innerHTML = data.sys.country
+
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+  }
+}
+
+checkWeather();
